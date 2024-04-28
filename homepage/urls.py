34c81c7a73942a278e0cd9d395a838d27f7f915a
Hilmi Atha Putra <108039453+hilmiatha@users.noreply.github.com>
2024-04-28 11:43:57 +0700

    added login and register

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import homepage_view
+from .views import *
 
 urlpatterns = [
     path('', homepage_view, name='homepage'),
+    path('login/', show_login, name='show_login'),
+    path('register/', show_register, name='show_register'),
+    path('register/show_register_pengguna/', show_register_pengguna, name='show_register_pengguna'),
+    path('register/show_register_label/', show_register_label, name='show_register_label'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render
 from django.db import connection
 from homepage.query import *
 from utils import parse
+from django.views.decorators.csrf import csrf_exempt
+
 
 def homepage_view(request):
-    with connection.cursor() as cursor:
-        cursor.execute(select_all())
-        result = cursor.fetchall()
-    
-    return render(request, 'index.html', {'data': result})
+    return render(request, 'homepage.html')
+
+
+
+@csrf_exempt
+def show_login(request):
+    return render(request, 'login.html')
+
+
+@csrf_exempt
+def show_register(request):
+    return render(request, 'register.html')
+
+@csrf_exempt
+def show_register_pengguna(request):
+    return render(request, 'register_pengguna.html')
+
+
+@csrf_exempt
+def show_register_label(request):
+    return render(request, 'register_label.html')
