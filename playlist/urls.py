ec2cdada8d9bd4db771f3d632b71956ce3efea32
Hilmi Atha Putra <108039453+hilmiatha@users.noreply.github.com>
2024-05-01 14:07:26 +0700

    Finsihed FE fitur ijo

---
+++ playlist/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', show_playlist, name='show_playlist'),
+    path('buat_playlist/', buat_playlist, name='buat_playlist'),
+    path('playlist_detail/<uuid:id_playlist>/', playlist_detail, name='playlist_detail'),
+    path('tambah_lagu_to_playlist/<uuid:id_playlist>/', tambah_lagu_to_playlist, name='tambah_lagu_to_playlist'),
+    path('ubah_playlist/<uuid:id_playlist>/', show_ubah_playlist, name='ubah_playlist'),
+    path('show_song/<uuid:id_song>/', show_song_detailed, name='show_song'),
+    path('add_song_to_playlist/<uuid:id_song>', add_song_to_playlist, name='add_song_to_playlist'),
+]
