4d7a3636c9a9f8731013cbe2916026de0b562829
RizvanuSN <rizvanusn@gmail.com>
2024-05-02 01:53:12 +0700

    fix some things

--- chart/urls.py
+++ chart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-   
+   path('see_chart/', see_chart, name='see_chart'),
+   path('see_songs_chart/', see_songs_chart, name='see_songs_chart')
 ]
--- chart/views.py
+++ chart/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def see_chart(request):
+    return render(request, 'see_chart.html')
+
+
+def see_songs_chart(request):
+    return render(request, 'see_songs_chart.html')
