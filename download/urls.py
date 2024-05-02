d1c1e27fc48e5c4ed8814aa7578d0e7cd30839a1
nabielsubhan <mnsubhan1509@gmail.com>
2024-05-02 21:01:02 +0700

    create downloaded songs page

--- download/urls.py
+++ download/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-   
+   path('downloaded_songs/', downloaded_songs, name='downloaded_songs'),
 ]
--- download/views.py
+++ download/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def downloaded_songs(request):
+    return render(request, 'downloaded_songs.html')
