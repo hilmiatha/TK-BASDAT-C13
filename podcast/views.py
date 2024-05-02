ddcb59c72ef4c29710d32df834c258dd1c09205b
Hilmi Atha Putra <108039453+hilmiatha@users.noreply.github.com>
2024-05-02 22:07:03 +0700

    nyamain dev

--- podcast/views.py
+++ podcast/views.py
@@ -7,8 +7,19 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 # @csrf_exempt
-def test_view(request):
-    return render(request, 'play_podcast.html')
+def play_podcast(request):
+    context = {
+        'is_logged_in' : True,
+        'user_type_info'  : {
+            'is_pengguna_biasa' : True,
+            'is_premium' : False,
+            'is_label' : False,
+            'is_podcaster' : False,
+            'is_artist' : False,
+            'is_songwriter' : False,
+        },  
+    }
+    return render(request, 'play_podcast.html',context)
 
 def manage_podcast(request):
     return render(request, 'manage_podcast.html')
