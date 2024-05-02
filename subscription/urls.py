08d71183096c24354221005ab981c76cd2f0912b
nabielsubhan <mnsubhan1509@gmail.com>
2024-05-02 17:04:11 +0700

    create langganan paket page

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-   
+   path('langganan_paket', langganan_paket, name='langganan_paket'),
+   path('pembayaran_paket', pembayaran_paket, name='pembayaran_paket'),
+   path('riwayat_paket', riwayat_paket, name='riwayat_paket'),
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def langganan_paket(request):
+    return render(request, 'langganan_paket.html')
+
+def pembayaran_paket(request):
+    return render(request, 'pembayaran_paket.html')
+
+def riwayat_paket(request):
+    return render(request, 'riwayat_paket.html')
