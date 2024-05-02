58431ac7b829f58939d7529ae693c771c7dcee22
RizvanuSN <rizvanusn@gmail.com>
2024-05-02 19:45:31 +0700

    fix views

--- podcast/urls.py
+++ podcast/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('test/', test_view, name='test'),
+    path('play_podcast/', play_podcast, name='test'),
     path('manage_podcast/', manage_podcast, name='manage_podcast'),
      path('manage_episode/', manage_episode, name='manage_episode'),
 ]
