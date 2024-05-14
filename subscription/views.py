70747dfdf5cb97e434f529680bd7226713509cb6
nabielsubhan <mnsubhan1509@gmail.com>
2024-05-15 06:43:51 +0700

    implement langganan paket

--- subscription/views.py
+++ subscription/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from utils import parse
+from django.db import connection
+from .query import *
 
 # Create your views here.
 def langganan_paket(request):
+    cursor = connection.cursor()
+    cursor.execute(get_paket())
+    result = parse(cursor)
+
+    for row in result:
+        row['harga'] = format_harga(row['harga'])
+    
+    print(result)
+
     context = {
         'is_logged_in' : True,
+        'semua_paket' : result,
         'user_type_info'  : {
             'is_pengguna_biasa' : True,
             'is_premium' : False,
@@ -14,6 +27,9 @@ def langganan_paket(request):
         },  
     }
     return render(request, 'langganan_paket.html',context)
+
+def format_harga(harga):
+    return f"Rp{harga:,.0f}".replace(",", ".")
 
 def pembayaran_paket(request):
     context = {
