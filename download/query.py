9ce75f4d66b359dca1e6be96cbbec6295ceb56d6
nabielsubhan <mnsubhan1509@gmail.com>
2024-05-17 21:36:52 +0700

    Done RD Downloaded Songs

--- download/query.py
+++ download/query.py
@@ -1,7 +1,13 @@
 def get_downloaded_songs(email):
-    return f"""SELECT K.judul, A.nama FROM DOWNLOADED_SONG DS, SONG S, ARTIST AR, KONTEN K, AKUN A 
-    WHERE DS.email_downloader = '{email}'
-    and DS.id_song = S.id_konten 
-    and S.id_konten = K.id 
+    return f"""SELECT K.judul, A.nama, K.id, DS.email_downloader, DS.email_downloader FROM DOWNLOADED_SONG DS, SONG S, ARTIST AR, KONTEN K, AKUN A 
+    WHERE DS.email_downloader = '{email}' 
+    AND DS.id_song = S.id_konten 
+    AND S.id_konten = K.id 
     AND S.id_artist = AR.id 
-    and AR.email_akun = A.email;"""
+    AND AR.email_akun = A.email;"""
+
+def delete_downloaded_song_query(id_song, email_downloader):
+    return f"""DELETE FROM DOWNLOADED_SONG WHERE id_song = '{id_song}' AND email_downloader = '{email_downloader}';"""
+
+def get_song_name(id_song):
+    return f"""SELECT judul FROM KONTEN WHERE id = '{id_song}';"""
--- download/urls.py
+++ download/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
    path('downloaded_songs/', downloaded_songs, name='downloaded_songs'),
+   path('delete_downloaded_song/<uuid:id_song>/<str:email_downloader>', delete_downloaded_song, name='delete_downloaded_song')
 ]
