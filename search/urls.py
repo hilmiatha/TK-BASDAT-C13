53700ada77193aa5e21ac36d528b480e95e06560
nabielsubhan <mnsubhan1509@gmail.com>
2024-05-17 16:47:49 +0700

    Done R Search Bar

--- search/urls.py
+++ search/urls.py
@@ -3,5 +3,4 @@ from .views import *
 
 urlpatterns = [
    path('hasil_pencarian/', hasil_pencarian, name='hasil_pencarian'),
-   path('tidak_ditemukan/', tidak_ditemukan, name='tidak_ditemukan'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,8 +1,28 @@
+from django.db import connection
 from django.shortcuts import render
+from utils import parse
+from .query import *
 
 # Create your views here.
 def hasil_pencarian(request):
-    return render(request, 'hasil_pencarian.html')
+    search_input = request.GET.get('query', '')
+    cursor = connection.cursor()
 
-def tidak_ditemukan(request):
-    return render(request, 'tidak_ditemukan.html')
+    cursor.execute(search_query_song(search_input))
+    song_result = parse(cursor)
+
+    cursor.execute(search_query_podcast(search_input))
+    podcast_result = parse(cursor)
+
+    cursor.execute(search_query_user_playlist(search_input))
+    user_playlist_result = parse(cursor)
+
+
+    context = {
+        'search_input' : search_input,
+        'song_result' : song_result,
+        'podcast_result' : podcast_result,
+        'user_playlist_result' : user_playlist_result
+    }
+    
+    return render(request, 'hasil_pencarian.html', context)
